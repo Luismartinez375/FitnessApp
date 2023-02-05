1590125c3f6e456ad1d1a4f7744d318e22931385
bear <manbearpig375@gmail.com>
2023-02-05 16:02:07 -0600

    added functionality, and added some backend stuff to app

--- fitness_app/lib/screens/loginScreen.dart
+++ fitness_app/lib/screens/loginScreen.dart
@@ -1,13 +1,18 @@
 import 'package:flutter/material.dart';
+import '../widgets/custom_textfield.dart';
 
 class LogIn extends StatefulWidget {
-  const LogIn({super.key});
+  static String routName = '/login-email-password';
+  const LogIn({Key? key}) : super(key: key);
 
   @override
   State<LogIn> createState() => _LogInState();
 }
 
 class _LogInState extends State<LogIn> {
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+  void loginUser() {}
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -33,7 +38,42 @@ class _LogInState extends State<LogIn> {
                     height: 300,
                   ))
             ],
-          ))
+          )),
+          SizedBox(height: MediaQuery.of(context).size.height * 0.08),
+          Container(
+            margin: const EdgeInsets.symmetric(horizontal: 20),
+            child: CustomTextField(
+              controller: emailController,
+              hintText: 'Enter your email',
+            ),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          Container(
+            margin: const EdgeInsets.symmetric(horizontal: 20),
+            child: CustomTextField(
+              controller: passwordController,
+              hintText: 'Enter your password',
+            ),
+          ),
+          const SizedBox(
+            height: 40,
+          ),
+          ElevatedButton(
+            onPressed: loginUser,
+            style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all(Colors.red),
+                textStyle: MaterialStateProperty.all(
+                  const TextStyle(color: Colors.white),
+                ),
+                minimumSize: MaterialStateProperty.all(
+                    Size(MediaQuery.of(context).size.width / 2.5, 50))),
+            child: const Text(
+              'Login',
+              style: TextStyle(color: Colors.white, fontSize: 16),
+            ),
+          )
         ]),
       )),
     );
