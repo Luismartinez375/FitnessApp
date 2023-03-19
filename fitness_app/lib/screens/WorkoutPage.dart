135fc780b1727c50a07dc0f246cc175a0162d794
ccano04 <124080006+ccano04@users.noreply.github.com>
2023-03-19 13:31:22 -0500

    new images

--- fitness_app/lib/screens/WorkoutPage.dart
+++ fitness_app/lib/screens/WorkoutPage.dart
@@ -1,175 +1,8 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-
-// // class WorkoutPage extends StatelessWidget {
-// //   final List<Map<String, dynamic>> categories = [    {      'name': 'Chest',      'image': 'assets/images/chest-main.jpg',      'subcategories': [        {          'name': 'Bench Press',          'image': 'assets/images/chest/bench_press.jpg',        },        {          'name': 'Cable Cross Over',          'image': 'assets/images/chest/cable_cross_over.jpg',        },      ],
-// //     },
-// //     {
-// //       'name': 'Back',
-// //       'image': 'assets/images/back-main.jpg',
-// //       'subcategories': [        {          'name': 'Lat Pull-Down',          'image': 'assets/images/back/lat_pull_down.jpg',        },        {          'name': 'Pull-Up',          'image': 'assets/images/back/pull_up.jpg',        },      ],
-// //     },
-// //   ];
-
-//  import 'package:flutter/material.dart';
-
-// class WorkoutPage extends StatelessWidget {
-
-//   final List<Map<String, dynamic>> categories = [   
-//      {      
-//       'name': 'Chest','image': 'assets/images/chest_main.jpg', 
-//       'subcategories': [
-//         {'name': 'Bench Press','image': 'assets/images/chest/bench_press.jpg',},        
-//       { 'name': 'Cable Cross Over', 'image': 'assets/images/chest/cable_cross_over.jpg',},      
-//       ],
-//      },
-//      {
-//        'name': 'Back','image': 'assets/images/back_main.jpg',
-//        'subcategories': 
-//        [
-//         {'name': 'Lat Pull-Down','image': 'assets/images/back/lat_pull_down.jpg',},
-//        {'name': 'Pull-Up', 'image': 'assets/images/back/pull_up.jpg',},
-//        ],
-//      },
-//  ];
-
-//    WorkoutPage({super.key});
-
-//  @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Text('Exercises'),
-//       ),
-//       body: GridView.builder(
-//         padding: EdgeInsets.all(10),
-//         itemCount: categories.length,
-//         gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-//           crossAxisCount: 2,
-//           crossAxisSpacing: 10,
-//           mainAxisSpacing: 10,
-//         ),
-//         itemBuilder: (BuildContext context, int index) {
-//           return GestureDetector(
-//             onTap: () {
-//               Navigator.push(
-//                 context,
-//                 MaterialPageRoute(
-//                   builder: (context) => SubcategoryPage(
-//                     category: categories[index],
-//                   ),
-//                 ),
-//               );
-//             },
-//             child: Container(
-//               decoration: BoxDecoration(
-//                 borderRadius: BorderRadius.circular(10),
-//                 image: DecorationImage(
-//                   image: AssetImage(categories[index]['image']),
-//                   fit: BoxFit.cover,
-//                 ),
-//               ),
-//               child: Column(
-//                 mainAxisAlignment: MainAxisAlignment.end,
-//                 crossAxisAlignment: CrossAxisAlignment.center,
-//                 children: [
-//                   Text(
-//                     categories[index]['name'],
-//                     style: TextStyle(
-//                       color: Colors.white,
-//                       fontWeight: FontWeight.bold,
-//                       fontSize: 24,
-//                     ),
-//                   ),
-//                 ],
-//               ),
-//             ),
-//           );
-//         },
-//       ),
-//     );
-//   }
-// }
-
-
-// class SubcategoryPage extends StatefulWidget {
-//   final Map<String, dynamic> category;
-
-//   SubcategoryPage({required this.category});
-
-//   @override
-//   _SubcategoryPageState createState() => _SubcategoryPageState();
-// }
-
-// class _SubcategoryPageState extends State<SubcategoryPage> {
-//   bool _isLoading = true;
-
-//   @override
-//   void initState() {
-//     super.initState();
-//     WidgetsBinding.instance?.addPostFrameCallback((_) {
-//       setState(() {
-//         _isLoading = false;
-//       });
-//     });
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     List<Map<String, dynamic>> subcategories = widget.category['subcategories'];
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Text(widget.category['name']),
-//       ),
-//       body: _isLoading
-//           ? Center(
-//               child: CircularProgressIndicator(),
-//             )
-//           : GridView.builder(
-              
-//               padding: EdgeInsets.all(10),
-//               itemCount: subcategories.length,
-//               gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-//                 crossAxisCount: 3,
-//                 crossAxisSpacing: 10,
-//                 mainAxisSpacing: 10,
-//               ),
-//               itemBuilder: (BuildContext context, int index) {
-//           return GestureDetector(
-//             onTap: () {
-//               // TODO: Implement subcategory tap functionality
-//             },
-//             child: Container(
-//               decoration: BoxDecoration(
-//                 borderRadius: BorderRadius.circular(10),
-//                 image: DecorationImage(
-//                   image: AssetImage(subcategories[index]['image']),
-//                   fit: BoxFit.cover,
-//                 ),
-//               ),
-//               child: Column(
-//                 mainAxisAlignment: MainAxisAlignment.end,
-//                 crossAxisAlignment: CrossAxisAlignment.center,
-//                 children: [
-//                   Text(
-//                     subcategories[index]['name'],
-//                     style: TextStyle(
-//                       color: Colors.white,
-//                       fontWeight: FontWeight.bold,
-//                       fontSize: 24,
-//                     ),
-//                   ),
-//                 ],
-//               ),
-//             ),
-//           );
-//         },
-//       ),
-//     );
-//   }
-// }
-
+import 'package:fitness_app/screens/ExerciseListScreen.dart';
 
 class WorkoutPage extends StatefulWidget {
   @override
@@ -177,7 +10,7 @@ class WorkoutPage extends StatefulWidget {
 }
 
 class _WorkoutPageState extends State<WorkoutPage> {
-  final categories = ['Chest', 'Back', 'Bicep', 'Tricep', 'Abs', 'Shoulder', 'Legs'];
+  final daysOfWeek = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
 
   @override
   Widget build(BuildContext context) {
@@ -185,29 +18,64 @@ class _WorkoutPageState extends State<WorkoutPage> {
       appBar: AppBar(
         title: Text('Workout'),
       ),
-      body: GridView.builder(
-        padding: EdgeInsets.all(8),
-        itemCount: categories.length,
-        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2,
-          mainAxisSpacing: 8,
-          crossAxisSpacing: 8,
-        ),
-        itemBuilder: (BuildContext context, int index) {
-          return InkWell(
-            onTap: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => ExerciseListScreen(category: categories[index]),
+      body: StreamBuilder<QuerySnapshot>(
+        stream: getWorkouts(),
+        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          }
+
+          return ListView.builder(
+            itemCount: daysOfWeek.length,
+            itemBuilder: (BuildContext context, int index) {
+              final day = daysOfWeek[index];
+
+              // Filter workouts for the current day
+              final dailyWorkouts = snapshot.data!.docs.where((doc) {
+                final timestamp = doc['timestamp'] as Timestamp;
+                final date = timestamp.toDate();
+                return date.weekday == index + 1;
+              }).toList();
+
+              return Container(
+                margin: EdgeInsets.symmetric(vertical: 4, horizontal: 8),
+                padding: EdgeInsets.all(8),
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.grey),
+                  borderRadius: BorderRadius.circular(8),
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      day,
+                      style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                    ),
+                    if (dailyWorkouts.isNotEmpty)
+                      ...dailyWorkouts.map((workout) {
+                        return ListTile(
+                          leading: Image.network(workout['imageUrl']),
+                          title: Text('${workout['sets']} sets x ${workout['reps']} reps'),
+                        );
+                      }).toList()
+                    else
+                      Center(
+                        child: TextButton(
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => ExerciseListScreen(dayOfWeek: day),
+                              ),
+                            );
+                          },
+                          child: Text('Create Workout', textAlign: TextAlign.right),
+                        ),
+                      ),
+                  ],
                 ),
               );
             },
-            child: Card(
-              child: Center(
-                child: Text(categories[index]),
-              ),
-            ),
           );
         },
       ),
@@ -215,145 +83,13 @@ class _WorkoutPageState extends State<WorkoutPage> {
   }
 }
 
-class ExerciseListScreen extends StatefulWidget {
-  final String category;
-
-  ExerciseListScreen({required this.category});
-
-  @override
-  _ExerciseListScreenState createState() => _ExerciseListScreenState();
+Stream<QuerySnapshot> getWorkouts() {
+  final userId = FirebaseAuth.instance.currentUser!.uid;
+  return FirebaseFirestore.instance
+      .collection('users')
+      .doc(userId)
+      .collection('workouts')
+      .orderBy('timestamp', descending: true)
+      .snapshots();
 }
 
-class _ExerciseListScreenState extends State<ExerciseListScreen> {
-  // Replace these with your actual exercise image URLs
-  final exercises = {
-    'Chest': ['https://example.com/chest1.jpg', 'https://example.com/chest2.jpg'],
-    'Back': ['https://example.com/back1.jpg', 'https://example.com/back2.jpg'],
-    // ... other categories ...
-  };
-
-  @override
-  Widget build(BuildContext context) {
-    final exerciseImages = exercises[widget.category] ?? [];
-
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.category),
-      ),
-      body: GridView.builder(
-        padding: EdgeInsets.all(8),
-        itemCount: exerciseImages.length,
-        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2,
-          mainAxisSpacing: 8,
-          crossAxisSpacing: 8,
-        ),
-        itemBuilder: (BuildContext context, int index) {
-          return InkWell(
-            onTap: () {
-              _showExerciseDialog(exerciseImages[index]);
-            },
-            child: Card(
-              child: Image.network(exerciseImages[index], fit: BoxFit.cover),
-            ),
-          );
-        },
-      ),
-    );
-  }
-  
-Future<void> _showExerciseDialog(String imageUrl) async {
-    int sets = 1;
-    int reps = 1;
-
-    await showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return StatefulBuilder(
-          builder: (BuildContext context, StateSetter setState) {
-            return AlertDialog(
-              title: Text('Select sets and repetitions'),
-              content: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text('Sets:'),
-                      DropdownButton<int>(
-                        value: sets,
-                        items: List.generate(10, (index) => index + 1)
-                            .map<DropdownMenuItem<int>>(
-                              (int value) => DropdownMenuItem<int>(
-                                value: value,
-                                child: Text(value.toString()),
-                              ),
-                            )
-                            .toList(),
-                        onChanged: (int? newValue) {
-                          setState(() {
-                            sets = newValue!;
-                          });
-                        },
-                      ),
-                    ],
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text('Reps:'),
-                      DropdownButton<int>(
-                        value: reps,
-                        items: List.generate(30, (index) => index + 1)
-                            .map<DropdownMenuItem<int>>(
-                              (int value) => DropdownMenuItem<int>(
-                                value: value,
-                                child: Text(value.toString()),
-                              ),
-                            )
-                            .toList(),
-                        onChanged: (int? newValue) {
-                          setState(() {
-                            reps = newValue!;
-                          });
-                        },
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-              actions: [
-                TextButton(
-                  onPressed: () => Navigator.pop(context),
-                  child: Text('Close'),
-                ),
-                TextButton(
-                  onPressed: () async {
-                    await _saveExercise(imageUrl, sets, reps);
-                    Navigator.pop(context);
-                  },
-                  child: Text('Save'),
-                ),
-              ],
-            );
-          },
-        );
-      },
-    );
-  }
-
-  Future<void> _saveExercise(String imageUrl, int sets, int reps) async {
-    final userId = FirebaseAuth.instance.currentUser!.uid;
-
-    await FirebaseFirestore.instance
-        .collection('users')
-        .doc(userId)
-        .collection('workouts')
-        .add({
-          'imageUrl': imageUrl,
-          'sets': sets,
-          'reps': reps,
-          'timestamp': FieldValue.serverTimestamp(),
-        });
-  }
-}
--- fitness_app/lib/screens/loginScreen.dart
+++ fitness_app/lib/screens/loginScreen.dart
@@ -1,12 +1,14 @@
-import 'package:fitness_app/auth/firebaseAuthMethods.dart';
-import 'package:fitness_app/screens/homeScreen.dart';
-import 'package:fitness_app/widgets/googleSigninButton.dart';
-import 'package:fitness_app/widgets/snackBar.dart';
+import 'package:fitness_app/screens/registerScreen.dart';
 import 'package:flutter/material.dart';
-import '../widgets/custom_textfield.dart';
-import 'registerScreen.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:provider/provider.dart';
+import 'package:gymrat_example/services/firebase_auth_methods.dart';
+import 'package:gymrat_example/widgets/custom_text_field.dart';
+import 'package:gymrat_example/widgets/google_sign_in_button.dart';
+import 'package:gymrat_example/screens/email_password_signup.dart';
+
+import '../auth/firebaseAuthMethods.dart';
+import '../widgets/custom_textfield.dart';
+import '../widgets/googleSigninButton.dart';
 
 class LogIn extends StatefulWidget {
   static String routName = '/login-email-password';
@@ -30,94 +32,99 @@ class _LogInState extends State<LogIn> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        backgroundColor: Colors.redAccent,
-        body: ListView(
-          children: [
-            Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-              Padding(
-                padding: const EdgeInsets.only(
-                  left: 16.0,
-                  right: 16.0,
-                  bottom: 10.0,
+    return WillPopScope(
+      onWillPop: () async => false,
+      child: SafeArea(
+        child: Scaffold(
+          backgroundColor: Colors.redAccent,
+          body: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                // App Logo
+                Padding(
+                  padding: const EdgeInsets.only(
+                    top: 20.0,
+                    bottom: 10.0,
+                  ),
+                  child: CircleAvatar(
+                    backgroundImage: AssetImage('assets/gymrat.png'),
+                    radius: 150.0,
+                  ),
                 ),
-                child: Column(mainAxisSize: MainAxisSize.min, children: [
-                  Column(
-                    mainAxisSize: MainAxisSize.min,
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: [
-                      SizedBox(
-                        height: 20,
+
+                // Email Input
+                Container(
+                  margin: const EdgeInsets.symmetric(horizontal: 20),
+                  child: CustomTextField(
+                    controller: emailController,
+                    hintText: 'Enter your email',
+                  ),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+
+                // Password Input
+                Container(
+                  margin: const EdgeInsets.symmetric(horizontal: 20),
+                  child: CustomTextField(
+                    controller: passwordController,
+                    hintText: 'Enter your password',
+                  ),
+                ),
+                const SizedBox(
+                  height: 40,
+                ),
+
+                // Login Button
+                ElevatedButton(
+                  onPressed: () {
+                    loginUser();
+                  },
+                  style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all(Colors.amber),
+                      textStyle: MaterialStateProperty.all(
+                        const TextStyle(color: Colors.white),
                       ),
-                      Flexible(
-                          flex: 1,
-                          child: CircleAvatar(
-                            backgroundImage: AssetImage('assets/gymrat.png'),
-                            radius: 150.0,
-                          ))
-                    ],
+                      minimumSize: MaterialStateProperty.all(
+                          Size(MediaQuery.of(context).size.width / 2.5, 50))),
+                  child: const Text(
+                    'Login',
+                    style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold, fontSize: 16),
                   ),
-                  SizedBox(height: MediaQuery.of(context).size.height * 0.08),
-                  Container(
-                    margin: const EdgeInsets.symmetric(horizontal: 20),
-                    child: CustomTextField(
-                      controller: emailController,
-                      hintText: 'Enter your email',
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  Container(
-                    margin: const EdgeInsets.symmetric(horizontal: 20),
-                    child: CustomTextField(
-                      controller: passwordController,
-                      hintText: 'Enter your password',
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 40,
-                  ),
-                  ElevatedButton(
-                    onPressed: () {
-                      loginUser();
+                ),
+
+                // Register Navigation
+                Padding(
+                  padding: const EdgeInsets.only(top: 10.0),
+                  child: InkWell(
+                    child: const Text('Dont have an account? Register here!',
+                        style: TextStyle(
+                            decoration: TextDecoration.underline,
+                            color: Colors.blue)),
+                    onTap: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) =>
+                                  const EmailPasswordSignup()));
                     },
-                    style: ButtonStyle(
-                        backgroundColor: MaterialStateProperty.all(Colors.amber),
-                        textStyle: MaterialStateProperty.all(
-                          const TextStyle(color: Colors.white),
-                        ),
-                        minimumSize: MaterialStateProperty.all(
-                            Size(MediaQuery.of(context).size.width / 2.5, 50))),
-                    child: const Text(
-                      'Login',
-                      style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold,fontSize: 16),
-                    ),
                   ),
-                  Padding(
-                    padding: const EdgeInsets.only(top: 10.0),
-                    child: InkWell(
-                      child: const Text('Dont have an account? Register here!',
-                          style: TextStyle(
-                              decoration: TextDecoration.underline,
-                              color: Colors.blue)),
-                      onTap: () {
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) =>
-                                    const EmailPasswordSignup()));
-                      },
-                    ),
-                  )
-                ]),
-              )
-            ]),
-            const SizedBox(
-              height: 20,
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+// Google Sign In Button
+                GoogleSignInButton(),
+              ],
             ),
-            GoogleSignInButton()
-          ],
-        ));
+          ),
+        ),
+      ),
+    );
   }
 }
+               
+
+
